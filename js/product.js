59288dd5b249c6c0f5a7f1485b758baf9c5b18ba
anashossainios16-create <anashossainios16@gmail.com>
2026-07-13 18:40:34 +0600

    Create product.js

---
+++ js/product.js
@@ -0,0 +1,31 @@
+const product = {
+
+    name: "KES Premium Cooker",
+
+    price: "৳ 1300",
+
+    image: "images/product.jpg",
+
+    description:
+    "Premium quality cooker. ৬ মাস warranty সহ। সারা বাংলাদেশে ক্যাশ অন ডেলিভারি সুবিধা।"
+
+};
+
+
+// Product show
+
+document.getElementById("product-name").innerText = product.name;
+
+document.getElementById("product-price").innerText = product.price;
+
+document.getElementById("product-description").innerText = product.description;
+
+document.getElementById("product-image").src = product.image;
+
+
+// Save product for order page
+
+localStorage.setItem(
+    "selectedProduct",
+    JSON.stringify(product)
+);
